aeea5304c83a7a0ad65e254b42dbaa29b16dbc36
PurityGwaro <puritygwaro@gmail.com>
2025-10-29 21:35:04 +0300

    error handling

--- index.js
+++ index.js
@@ -8,9 +8,24 @@ const port = process.env.PORT || 3006;
 
 app.use(cors());
 app.use(express.json());
-app.use('/countries', countryRoutes);
+
 app.get('/', (req, res) => {
- res.json({ message: 'Country Details API is running' });
+    res.json({ message: 'Country Details API is running' });
+});
+
+app.use('/countries', countryRoutes);
+
+app.use((req, res) => {
+    res.status(404).json({
+        message: 'Route not found'
+    });
+});
+
+app.use((err, req, res, next) => {
+    console.error('Unhandled error:', err);
+    res.status(500).json({
+        message: err.message
+    });
 });
 
 app.listen(port, () => {
